1b6ffcb2773e9b1bdeaa210e034c0b3c5beaf0a3
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2025-12-13 23:52:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Dog App
+
+* A website to demonstrate a general knowledge of APIs!
+* Utilizes three [TheDogAPI](https://thedogapi.com/) endpoints: images, breed groups, and breed purpose.
+
+## Instructions
+
+* Use the [GitHub page](https://evenstevenh.github.io/Dog-App/) to check it out!
+* No installations or API keys needed.
---
+++ app.js
@@ -0,0 +1,98 @@
+const header = document.querySelector("#header")
+
+window.addEventListener('load', function () {
+    header.innerHTML = `
+        <h1>Dog API</h1>
+        <nav>
+            <li><a href="index.html">Images</a></li>
+            <li><a href="dogBreed.html">Breeds</a></li>
+            <li><a href="dogPurpose.html">Purpose</a></li>
+            <li><a href="https://github.com/EvenStevenH/Dog-App" target="_blank">Github</a></li>
+        </nav>`
+})
+
+function getDogImage() {
+    fetch("https://api.thedogapi.com/v1/images/search?limit=1")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Could not fetch resource. Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const dogImage = data[0].url;
+            const imgElement = document.getElementById("dogImage");
+
+            if (imgElement) {
+                imgElement.src = dogImage;
+                imgElement.style.display = "block";
+            } else {
+                console.log('Image element not found.');
+            }
+        })
+        .catch(error => console.error("Error:", error.message));
+}
+
+function listDogBreed() {
+    const dogBreedInput = document.getElementById("dogBreed-select").value;
+
+    fetch(`https://api.thedogapi.com/v1/breeds?breed_groups=${dogBreedInput}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Could not fetch resource. Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const dogBreedList = document.getElementById("dogBreedList");
+            dogBreedList.innerHTML = '';
+
+            data.forEach(breed => {
+                const li = document.createElement('li');
+                li.textContent = breed.name;
+                dogBreedList.appendChild(li);
+            });
+        })
+        .catch(error => console.error("Error:", error.message));
+}
+
+function getDogPurpose() {
+    const dogBreedInput = document.getElementById("dogBreedInput").value.toLowerCase();
+
+    if (dogBreedInput === "") {
+
+        fetch(`https://api.thedogapi.com/v1/breeds`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Could not fetch resource. Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const dogBreedPurpose = document.getElementById("dogBreedPurpose");
+                dogBreedPurpose.innerHTML = '';
+
+                const matchingBreeds = data.filter(breed =>
+                    breed.name.toLowerCase().includes(dogBreedInput)
+                );
+
+                if (matchingBreeds.length === 0) {
+                    dogBreedPurpose.textContent = 'Not a valid breed!';
+                } else {
+                    matchingBreeds.forEach(breed => {
+                        const li = document.createElement('li');
+                        if (!breed.bred_for || breed.bred_for.trim() === '') {
+                            li.textContent = `${breed.name}: purpose unknown. Let dogs be dogs!`;
+                        } else {
+                            li.textContent = `${breed.name}: ${breed.bred_for.toLowerCase()}`;
+                        }
+                        dogBreedPurpose.appendChild(li);
+                    });
+                }
+            })
+            .catch(error => {
+                console.error("Error:", error.message);
+                document.getElementById("dogBreedPurpose").textContent = 'Invalid breed, or breed does not have a known purpose.';
+            });
+    }
+}
